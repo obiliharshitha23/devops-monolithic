ba887f13cc07151b9ab354c2d3cfdebeba9cfc18
obiliharshitha23 <124793355+obiliharshitha23@users.noreply.github.com>
2026-09-15 20:19:00 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafa77.flm77.moonobucket"
+  bucket = "harshi-monobucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -22,13 +22,4 @@ versioning_configuration {
 status = "Enabled"
 }
 }
-
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "mustafa77.flm77.moonobucket"
-key = "prod/terraform.tfstate"
-}
-}
-
 
